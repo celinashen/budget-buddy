0a30ed859b6326eea22df6639f0e99dee1a6ba62
Celina <celina.shen06@gmail.com>
2021-01-16 20:07:33 -0500

    Created contentScript

--- background.js
+++ background.js
@@ -12,6 +12,7 @@ var pageConditions = {
     conditions: [
       new chrome.declarativeContent.PageStateMatcher({
         pageUrl: { urlContains: "/cart" } 
+        //pageUrl:{ schemes: ['https','http'] }
       })
     ],
     actions: [
@@ -34,20 +35,44 @@ var pageConditions = {
       window.open("./popup/popup.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
   }**/
 
+  
+ 
+
 
   chrome.tabs.onUpdated.addListener(function
     (tabId, changeInfo, tab) {
       // read changeInfo data and do something with it (like read the url)
-      let condition = changeInfo.url.includes('/cart')
+      
+
+      let condition = false;
+
+      if (tab.url){
+        condition = tab.url.includes('/cart')
+        
+      }
+      console.log(tab);
+      
       console.log(`condition=${condition}`)  
   
+      
+
       if (condition) {
-         window.open("./popup/popup.html", "extension_popup", "width=700,height=640,status=no,scrollbars=yes,resizable=yes");
-
+        console.log('cart found in url');
         
+        // var search = document.querySelector("a-size-medium a-color-base sc-price sc-white-space-nowrap");
+        // console.log(search)
 
+        window.open("./popup/popup.html", "extension_popup", "width=700,height=640,status=no,scrollbars=yes,resizable=yes");
+        
+        condition = false;
+
+        chrome.tabs.executeScript({
+          file: 'contentScript.js'
+        });
 
 
       }
     }
   );
+
+  
---
+++ contentScript.js
@@ -0,0 +1,3 @@
+
+
+console.log(document.querySelector('#sc-subtotal-amount-activecart > span').textContent.split('$')[1].trim())
